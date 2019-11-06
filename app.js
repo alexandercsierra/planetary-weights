7a6820ff69db9addebea13c3b484bf68e9ef3d85
Alexander Sierra <alexandercsierra@gmail.com>
2019-11-06 04:20:21 -0500

    images change with planet selection

--- app.js
+++ app.js
@@ -3,16 +3,50 @@ var weightSubmit = document.querySelector("button");
 var h2 = document.querySelector("h2");
 var result = document.querySelector(".result");
 var planet = document.querySelector("select");
+var body = document.querySelector("body");
 var weight = 0;
 
 weightSubmit.addEventListener("click", function (e){
     e.preventDefault();
     var planetChoice = planet.value;
+    console.log(planetChoice);
     var weight = weightInput.value;
-    var newWeight = spaceWeights("earth", weight, planetChoice);
-    planetChoice = planetChoice.charAt(0).toUpperCase() + planetChoice.slice(1);
-    console.log(weight);
+    var newWeight = spaceWeights("Earth", weight, planetChoice);
     h2.textContent = "You would weigh " + newWeight + " pounds on " + planetChoice;
+    
+    //change background according to planet choice
+
+    if (planetChoice === "Mercury"){
+        body.style.backgroundImage = "url('images/mercury.jpg')";
+    }
+    if (planetChoice === "Venus"){
+        body.style.backgroundImage = "url('images/venus.jpg')";
+    }
+    if (planetChoice === "Earth"){
+        body.style.backgroundImage = "url('images/earth1.jpg')";
+    }
+    if (planetChoice === "Mars"){
+        body.style.backgroundImage = "url('images/mars.jpg')";
+    }
+    if (planetChoice === "Jupiter"){
+        body.style.backgroundImage = "url('images/jupiter.jpg')";
+    }
+    if (planetChoice === "Saturn"){
+        body.style.backgroundImage = "url('images/saturn.jpg')";
+    }
+    if (planetChoice === "Uranus"){
+        // body.style.backgroundImage = "url('images/uranus.jpg')";
+        body.style.background = "#000000 url('images/uranus.jpg')";
+        body.style.backgroundPosition = "center";
+        body.style.backgroundSize = "60%"
+        body.style.backgroundRepeat = "no-repeat"
+    }
+    if (planetChoice === "Neptune"){
+        body.style.backgroundImage = "url('images/neptune.jpg')";
+    }
+    if (planetChoice === "Pluto"){
+        body.style.backgroundImage = "url('images/pluto.jpg')";
+    }
 });
 
 // weightInput.addEventListener("keypress", function (e){
@@ -27,15 +61,15 @@ weightSubmit.addEventListener("click", function (e){
 
 function spaceWeights(planetA, weight, planetB) {
   var planetGravs = {
-    mercury:3.7,
-    venus:8.87,
-    earth:9.81,
-    mars:3.711,
-    jupiter:24.79,
-    saturn:10.44,
-    uranus:8.69,
-    neptune:11.15,
-    pluto: 0.62
+    Mercury:3.7,
+    Venus:8.87,
+    Earth:9.81,
+    Mars:3.711,
+    Jupiter:24.79,
+    Saturn:10.44,
+    Uranus:8.69,
+    Neptune:11.15,
+    Pluto: 0.62
   }
   var weight = (weight/planetGravs[planetA]) * planetGravs[planetB];
   return Number(weight.toFixed(2));
