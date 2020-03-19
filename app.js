c74a72ed0d28f00586f5cd47eb22402705d3d72b
Alexander Sierra <alexandercsierra@gmail.com>
2020-03-19 00:45:16 -0400

    updated styling

--- app.js
+++ app.js
@@ -22,69 +22,72 @@ weightSubmit.addEventListener("click", function (e){
 		var newWeight = spaceWeights("Earth", weight, planetChoice);
         h2.textContent = "You would weigh " + newWeight + " kilos on " + planetChoice;
     }
-	// var newWeight = spaceWeights("Earth", weight, planetChoice);
+	var newWeight = spaceWeights("Earth", weight, planetChoice);
 	
 
     
     
-    //change background according to planet choice
+    // change background according to planet choice
 
-    // if (planetChoice === "Mercury"){
-    //     body.style.background = "#000000 url('images/mercury.jpg')";
-    //     body.style.backgroundPosition = "cover";
-    //     body.style.backgroundSize = "1920px 1080px";
-    // }
-    // if (planetChoice === "Venus"){
-    //     body.style.background = "#000000 url('images/venus.jpg')";
-    //     body.style.backgroundPosition = "cover";
-    //     body.style.backgroundSize = "1920px 1080px";
-    // }
-    // if (planetChoice === "Earth"){
-    //     body.style.background = "#000000 url('images/earth1.jpg')";
-    //     body.style.backgroundPosition = "cover";
-    //     body.style.backgroundSize = "1920px 1080px";
-    // }
-    // if (planetChoice === "Mars"){
-    //     body.style.background = "#000000 url('images/mars.jpg')";
-    //     body.style.backgroundPosition = "cover";
-    //     body.style.backgroundSize = "1920px 1080px";
-    // }
-    // if (planetChoice === "Jupiter"){
-    //     body.style.background = "#000000 url('images/jupiter.jpg')";
-    //     body.style.backgroundPosition = "cover";
-    //     body.style.backgroundSize = "1920px 1080px";
-    // }
-    // if (planetChoice === "Saturn"){
-    //     body.style.background = "#000000 url('images/saturn.jpg')";
-    //     body.style.backgroundPosition = "cover";
-    //     body.style.backgroundSize = "1920px 1080px";
-    // }
-    // if (planetChoice === "Uranus"){
-    //     // body.style.backgroundImage = "url('images/uranus.jpg')";
-    //     body.style.background = "#000000 url('images/uranus.jpg')";
-    //     body.style.backgroundPosition = "center";
-    //     body.style.backgroundSize = "60%";
-    //     body.style.backgroundRepeat = "no-repeat";
-    // }
-    // if (planetChoice === "Neptune"){
-    //     body.style.background = "#000000 url('images/neptune.jpg')";
-    //     body.style.backgroundPosition = "cover";
-    //     body.style.backgroundSize = "1920px 1080px";
-    // }
-    // if (planetChoice === "Pluto"){
-    //     body.style.background = "#000000 url('images/pluto.jpg')";
-    //     body.style.backgroundPosition = "cover";
-    //     body.style.backgroundSize = "100%";
-    //     body.style.backgroundRepeat = "no-repeat";
-    // }
+    if (planetChoice === "Mercury"){
+        body.style.background = "#000000 url('images/mercury.jpg')";
+        // body.style.backgroundPosition = "cover";
+        // body.style.backgroundSize = "1920px 1080px";
+    }
+    if (planetChoice === "Venus"){
+        body.style.background = "#000000 url('images/venus.jpg')";
+        // body.style.backgroundPosition = "cover";
+        // body.style.backgroundSize = "1920px 1080px";
+    }
+    if (planetChoice === "Earth"){
+        body.style.background = "#000000 url('images/earth1.jpg')";
+        // body.style.backgroundPosition = "cover";
+        // body.style.backgroundSize = "1920px 1080px";
+    }
+    if (planetChoice === "Mars"){
+        body.style.background = "#000000 url('images/mars.jpg')";
+        // body.style.backgroundPosition = "cover";
+        // // body.style.backgroundSize = "1920px 1080px";
+    }
+    if (planetChoice === "Jupiter"){
+        body.style.background = "#000000 url('images/jupiter.jpg')";
+        // body.style.backgroundPosition = "cover";
+        // body.style.backgroundSize = "1920px 1080px";
+    }
+    if (planetChoice === "Saturn"){
+        body.style.background = "#000000 url('images/saturn.jpg')";
+        body.style.backgroundPosition = "30% 60%"
+    }
+    if (planetChoice === "Uranus"){
+        // body.style.backgroundImage = "url('images/uranus.jpg')";
+        body.style.background = "#000000 url('images/uranus.jpg')";
+        // body.style.backgroundPosition = "center";
+        // body.style.backgroundSize = "60%";
+        // body.style.backgroundRepeat = "no-repeat";
+    }
+    if (planetChoice === "Neptune"){
+        body.style.background = "#000000 url('images/neptune.jpg')";
+        // body.style.backgroundPosition = "cover";
+        // body.style.backgroundSize = "1920px 1080px";
+        // body.style.backgroundPosition = "center"
+        body.style.backgroundSize = "800px 800px";
+        body.style.backgroundRepeat = "no-repeat";
+        // body.style.backgroundPosition = "30% 60%"
+    }
+    if (planetChoice === "Pluto"){
+        body.style.background = "#000000 url('images/pluto.jpg')";
+        // body.style.backgroundPosition = "cover";
+        // body.style.backgroundSize = "100%";
+        // body.style.backgroundRepeat = "no-repeat";
+    }
 });
 
-// weightInput.addEventListener("keypress", function (e){
-//     if (e.key === 'Enter'){
-//         var newWeight = spaceWeights("Earth", weightInput.value, "Jupiter");
-//         console.log(newWeight);
-//     }  
-// });
+weightInput.addEventListener("keypress", function (e){
+    if (e.key === 'Enter'){
+        var newWeight = spaceWeights("Earth", weightInput.value, "Jupiter");
+        console.log(newWeight);
+    }  
+});
 
 
 
